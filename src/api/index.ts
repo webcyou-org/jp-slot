748ffdcf4647fa35f1fa1fa08aca3768d8219ba3
panicdragon <d.takayama.web@gmail.com>
2022-04-30 00:13:23 +0900

    feat: 🎸 create minorRole prams

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,15 @@
-import { BEING_ELECTED_LIST, BEING_ELECTED_WEIGHT_LIST, weightedRandom } from '../utils'
+import {
+    BEING_ELECTED_LIST,
+    BEING_ELECTED_WEIGHT_LIST,
+    MINOR_ROLE_BEING_ELECTED_LIST,
+    MINOR_ROLE_BEING_ELECTED_WEIGHT_LIST,
+    weightedRandom
+} from '../utils'
 
 export function drawing() {
     return weightedRandom(BEING_ELECTED_LIST, BEING_ELECTED_WEIGHT_LIST);
+}
+
+export function minorRoleDrawing() {
+    return weightedRandom(MINOR_ROLE_BEING_ELECTED_LIST, MINOR_ROLE_BEING_ELECTED_WEIGHT_LIST);
 }
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -12,7 +12,17 @@ export const BEING_ELECTED_LIST: string[] = [
     '当選無し'
 ];
 
+export const MINOR_ROLE_BEING_ELECTED_LIST: string[] = [
+    'スイカ当選',
+    'チェリー当選',
+    'ベル当選',
+    'リプレイ当選',
+    '当選無し'
+];
+
 export const BEING_ELECTED_WEIGHT_LIST: number[] = [5, 4, 50, 40, 170, 150, 1220, 610, 7710, 8978, 46599];
+
+export const MINOR_ROLE_BEING_ELECTED_WEIGHT_LIST: number[] = [1310, 930, 7710, 8987, 46599];
 
 export enum BonusFlag {
     Lose,
