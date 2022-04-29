a277a2d7a8b5c6c52cbfc149d9d4fe15485c3644
panicdragon <d.takayama.web@gmail.com>
2022-04-29 19:17:08 +0900

    feat: 🎸 add const & weightedRandom

---
+++ src/utils/const.ts
@@ -0,0 +1,15 @@
+export const BEING_ELECTED_LIST: string[] = [
+    'BIG当選',
+    'REG当選',
+    'BIG＋スイカ当選',
+    'REG＋スイカ当選',
+    'BIG＋チェリー当選',
+    'REG＋チェリー当選',
+    'スイカ当選',
+    'チェリー当選',
+    'ベル当選',
+    'リプレイ当選',
+    '当選無し'
+];
+
+export const BEING_ELECTED_WEIGHT_LIST: number[] = [5, 4, 50, 40, 170, 150, 1220, 610, 7710, 8978, 46599];
